b1799a49e8f0e3d187f01ad4b2f4682f5ba9267f
repiyann <muhrepiyan26@gmail.com>
2024-06-12 13:13:33 +0700

    fix(footer): fix footer to stay at bottom

--- src/components/mollecules/Footer.tsx
+++ src/components/mollecules/Footer.tsx
@@ -1,8 +1,14 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 export default function Footer() {
+	const pathname = usePathname()
+	const isHomeOrContact = pathname === '/' || pathname === '/contact'
+
 	return (
-		<footer className="dark:bg-[#121212] bg-white py-1 bottom-0 w-full">
+		<footer className={`dark:bg-[#121212] bg-white py-1 bottom-0 w-full ${isHomeOrContact && 'fixed'}`}>
 			<p className="text-center dark:text-white">Copyright © 2024 - All Right Reserved by Muhamad Repiyan Riski.</p>
 		</footer>
 	)
